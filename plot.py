b6e55bf3b40c7ef0dcbc8247ae245fc5ae60130f
hanifmz07 <13521157@std.stei.itb.ac.id>
2023-03-01 08:26:42 +0700

    add plot

---
+++ plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def visualize3D(arr, pair):
+    
+    x = arr[:, 0]
+    y = arr[:, 1]
+    z = arr[:, 2]
+    
+    fig = plt.figure(figsize=(8,8))
+    fig.suptitle('Closest Point in 3D plot')
+    ax = plt.axes(projection="3d")
+    
+    fg = ax.scatter3D(x, y, z, alpha = 0.5)
+    fg = ax.scatter3D(pair[0][0], pair[0][1], pair[0][2], color='r')
+    fg = ax.scatter3D(pair[1][0], pair[1][1], pair[1][2], color='r')
+    
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_label("z")
+    
+    plt.show()
